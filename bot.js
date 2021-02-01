a35a7b818ca16e019c8385d019a688dd7290b142
Reall BAD BOY <73025900+reall-bad-boy@users.noreply.github.com>
2021-02-02 02:25:39 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,404 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const { Client, Util } = require("discord.js");
+const http = require("http");
+const express = require("express");
+const request = require("request");
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+const client26 = new Discord.Client();
+const client27 = new Discord.Client();
+const client28 = new Discord.Client();
+const client29 = new Discord.Client();
+const client30 = new Discord.Client();
+const client31 = new Discord.Client();
+const client32 = new Discord.Client();
+const client33 = new Discord.Client();
+const client34 = new Discord.Client();
+const client35 = new Discord.Client();
+const client36 = new Discord.Client();
+const client37 = new Discord.Client();
+const client38 = new Discord.Client();
+const client39 = new Discord.Client();
+const client40 = new Discord.Client();
+const client41 = new Discord.Client();
+const client42 = new Discord.Client();
+const client43 = new Discord.Client();
+const client44 = new Discord.Client();
+const client45 = new Discord.Client();
+const client46 = new Discord.Client();
+const client47 = new Discord.Client();
+const client48 = new Discord.Client();
+const client49 = new Discord.Client();
+const client50 = new Discord.Client();
+
+
+
+
+
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping tamamdır.");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
+
+
+
+
+
+
+
+
+
+client.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 1`);
+  console.log(`Hi ${client} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client.guilds.size} " ]`);
+});
+client2.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 2 `);
+  console.log(`Hi ${client2} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client2.guilds.size} " ]`);
+});
+client3.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 3`);
+  console.log(`Hi ${client3} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client3.guilds.size} " ]`);
+});
+client4.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 4`);
+  console.log(`Hi ${client4} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client4.guilds.size} " ]`);
+});
+client5.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 5`);
+  console.log(`Hi ${client5} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client5.guilds.size} " ]`);
+});
+client6.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 6`);
+  console.log(`Hi ${client6} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client6.guilds.size} " ]`);
+});
+client7.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 7`);
+  console.log(`Hi ${client7} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client7.guilds.size} " ]`);
+});
+client8.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 8`);
+  console.log(`Hi ${client8} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client8.guilds.size} " ]`);
+});
+client9.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 9`);
+  console.log(`Hi ${client9} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client9.guilds.size} " ]`);
+});
+client10.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 10`);
+  console.log(`Hi ${client10} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client10.guilds.size} " ]`);
+});
+client11.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 11`);
+  console.log(`Hi ${client11} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client11.guilds.size} " ]`);
+});
+client12.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 12`);
+  console.log(`Hi ${client12} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client12.guilds.size} " ]`);
+});
+client13.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 13`);
+  console.log(`Hi ${client13} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client13.guilds.size} " ]`);
+});
+client14.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 14`);
+  console.log(`Hi ${client14} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client14.guilds.size} " ]`);
+});
+client15.on("ready", () => {
+  console.log(`[client] : ئەکاونتی15`);
+  console.log(`Hi ${client15} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client15.guilds.size} " ]`);
+});
+client16.on("ready", () => {
+  console.log(`[client] : ئەکاونتی16`);
+  console.log(`Hi ${client16} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client16.guilds.size} " ]`);
+});
+client17.on("ready", () => {
+  console.log(`[client] : ئەکاونتی17`);
+  console.log(`Hi ${client17} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client17.guilds.size} " ]`);
+});
+client18.on("ready", () => {
+  console.log(`[client] : ئەکاونتی18`);
+  console.log(`Hi ${client18} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client18.guilds.size} " ]`);
+});
+client19.on("ready", () => {
+  console.log(`[client] : ئەکاونتی19`);
+  console.log(`Hi ${client19} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client19.guilds.size} " ]`);
+});
+client20.on("ready", () => {
+  console.log(`[client] : ئەکاونتی20`);
+  console.log(`Hi ${client20} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client20.guilds.size} " ]`);
+});
+client21.on("ready", () => {
+  console.log(`[client] : ئەکاونتی21`);
+  console.log(`Hi ${client21} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client21.guilds.size} " ]`);
+});
+client22.on("ready", () => {
+  console.log(`[client] : ئەکاونتی22`);
+  console.log(`Hi ${client22} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client22.guilds.size} " ]`);
+});
+client23.on("ready", () => {
+  console.log(`[client] : ئەکاونتی23`);
+  console.log(`Hi ${client23} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client33.guilds.size} " ]`);
+});
+
+client24.on("ready", () => {
+  console.log(`[client] : ئەکاونتی24`);
+  console.log(`Hi ${client24} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client24.guilds.size} " ]`);
+});
+client25.on("ready", () => {
+  console.log(`[client] : ئەکاونتی25`);
+  console.log(`Hi ${client25} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client25.guilds.size} " ]`);
+});
+client26.on("ready", () => {
+  console.log(`[client] : ئەکاونتی26`);
+  console.log(`Hi ${client26} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client26.guilds.size} " ]`);
+});
+client27.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 27`);
+  console.log(`Hi ${client27} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client27.guilds.size} " ]`);
+});
+client28.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 28`);
+  console.log(`Hi ${client28} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client28.guilds.size} " ]`);
+});
+client29.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 29`);
+  console.log(`Hi ${client29} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client29.guilds.size} " ]`);
+});
+client30.on("ready", () => {
+  console.log(`[client] : الحساب رقم 30 يعمل`);
+  console.log(`Hi ${client30} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client30.guilds.size} " ]`);
+});
+client31.on("ready", () => {
+  console.log(`[client] : 31 ئەکاونتی `);
+  console.log(`Hi ${client31} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client31.guilds.size} " ]`);
+});
+client32.on("ready", () => {
+  console.log(`[client] : ئەکاونتی 32 `);
+  console.log(`Hi ${client32} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client32.guilds.size} " ]`);
+});
+client33.on("ready", () => {
+  console.log(`[client] : 33ئەکاونتی `);
+  console.log(`Hi ${client33} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client33.guilds.size} " ]`);
+});
+client34.on("ready", () => {
+  console.log(`[client] : ئەکاونتی34 `);
+  console.log(`Hi ${client34} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client34.guilds.size} " ]`);
+});
+client35.on("ready", () => {
+  console.log(`[client] : ئەکاونتی35 `);
+  console.log(`Hi ${client35} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client35.guilds.size} " ]`);
+});
+client36.on("ready", () => {
+  console.log(`[client] : 36ئەکاونتی `);
+  console.log(`Hi ${client36} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client36.guilds.size} " ]`);
+});
+client37.on("ready", () => {
+  console.log(`[client] :37 ئەکاونتی `);
+  console.log(`Hi ${client37} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client37.guilds.size} " ]`);
+});
+client38.on("ready", () => {
+  console.log(`[client] : 38ئەکاونتی `);
+  console.log(`Hi ${client38} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client38.guilds.size} " ]`);
+});
+client39.on("ready", () => {
+  console.log(`[client] : 39ئەکاونتی `);
+  console.log(`Hi ${client39} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client39.guilds.size} " ]`);
+});
+client40.on("ready", () => {
+  console.log(`[client] :40 ئەکاونتی `);
+  console.log(`Hi ${client40} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client40.guilds.size} " ]`);
+});
+client41.on("ready", () => {
+  console.log(`[client] : 41ئەکاونتی `);
+  console.log(`Hi ${client41} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client41.guilds.size} " ]`);
+});
+client42.on("ready", () => {
+  console.log(`[client] : 42ئەکاونتی `);
+  console.log(`Hi ${client42} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client42.guilds.size} " ]`);
+});
+client43.on("ready", () => {
+  console.log(`[client] : 43ئەکاونتی `);
+  console.log(`Hi ${client43} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client4.guilds.size} " ]`);
+});
+client44.on("ready", () => {
+  console.log(`[client] : 44ئەکاونتی `);
+  console.log(`Hi ${client44} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client44.guilds.size} " ]`);
+});
+client45.on("ready", () => {
+  console.log(`[client] : ئەکاونتی45 `);
+  console.log(`Hi ${client4} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client45.guilds.size} " ]`);
+});
+client46.on("ready", () => {
+  console.log(`[client] : 46ئەکاونتی `);
+  console.log(`Hi ${client46} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client46.guilds.size} " ]`);
+});
+client47.on("ready", () => {
+  console.log(`[client] :47 ئەکاونتی `);
+  console.log(`Hi ${client47} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client4.guilds.size} " ]`);
+});
+client48.on("ready", () => {
+  console.log(`[client] :48 ئەکاونتی `);
+  console.log(`Hi ${client48} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client48.guilds.size} " ]`);
+});
+client49.on("ready", () => {
+  console.log(`[client] : 49ئەکاونتی `);
+  console.log(`Hi ${client49} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client49.guilds.size} " ]`);
+});
+client50.on("ready", () => {
+  console.log(`[client] :50 ئەکاونتی `);
+  console.log(`Hi ${client50} , This Code by : HAMA `);
+  console.log(`i Have  [ " ${client50.guilds.size} " ]`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login("");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+client7.login("");
+client8.login("");
+client9.login("");
+client10.login("");
+client11.login("");
+client12.login("");
+client13.login("");
+client14.login("");
+client15.login("");
+client16.login("");
+client17.login("");
+client18.login("");
+client19.login("");
+client20.login("");
+client21.login("");
+client22.login("");
+client23.login("");
+client24.login("");
+client25.login("");
+client26.login("");
+client27.login("");
+client28.login("");
+client29.login("");
+client30.login("");
+client31.login("");
+client32.login("");
+client33.login("");
+client34.login("");
+client35.login("");
+client36.login("");
+client37.login("");
+client38.login("");
+client39.login("");
+client40.login("");
+client41.login("");
+client42.login("");
+client43.login("");
+client44.login("");
+client45.login("");
+client46.login("");
+client47.login("");
+client48.login("");
+client49.login("");
+client50.login("");
